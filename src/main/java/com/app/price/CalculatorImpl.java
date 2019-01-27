2a84415469a1d0be3420c1c648ba57735dc5a081
Sahil <sahilmalhotra15@gmail.com>
2019-01-27 21:03:12 +0000

    Initial testCase

---
+++ src/main/java/com/app/price/CalculatorImpl.java
@@ -0,0 +1,11 @@
+package com.app.price;
+
+import com.app.price.pricing.MarketUpdate;
+import com.app.price.pricing.TwoWayPrice;
+
+public class CalculatorImpl implements Calculator {
+    @Override
+    public TwoWayPrice applyMarketUpdate(MarketUpdate twoWayMarketPrice) {
+        return null;
+    }
+}
--- src/test/java/com/app/price/CalculatorTest.java
+++ src/test/java/com/app/price/CalculatorTest.java
@@ -1,5 +1,12 @@
 package com.app.price;
 
+import com.app.price.entity.Instrument;
+import com.app.price.entity.Market;
+import com.app.price.entity.State;
+import com.app.price.pricing.MarketUpdate;
+import com.app.price.pricing.TwoWayPrice;
+import com.app.price.pricing.impl.MarketUpdateImpl;
+import com.app.price.pricing.impl.TwoWayPriceImpl;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -7,6 +14,27 @@ import static org.junit.Assert.*;
 public class CalculatorTest {
 
     @Test
-    public void applyMarketUpdate() {
+    public void applyMarketUpdateTest() {
+
+        Calculator calculator = new CalculatorImpl();
+
+        MarketUpdate marketUpdate1 = new MarketUpdateImpl(getMarket1(), getTwoWayPrice1()) ;
+        TwoWayPrice twoWayPrice = calculator.applyMarketUpdate(marketUpdate1);
+        assertEquals("[INSTRUMENT1, bid_wvap=10.00, bid_amount=200.00, offer_wvap=11.00, offer_amount=1000.00]", format(twoWayPrice));
     }
+
+    private TwoWayPrice getTwoWayPrice1() {
+        return new TwoWayPriceImpl(Instrument.INSTRUMENT1, State.FIRM, 10,100, 11, 500);
+
+    }
+
+    private Market getMarket1() {
+        return Market.MARKET1;
+    }
+
+    private static String format(TwoWayPrice twp) {
+        return String.format("[%s, bid_wvap=%.2f, bid_amount=%.2f, offer_wvap=%.2f, offer_amount=%.2f]",
+                twp.getInstrument(), twp.getBidPrice(), twp.getBidAmount(), twp.getOfferPrice(), twp.getOfferAmount());
+    }
+
 }
